51cdc70767f259943824c6f1a25b0d54d54d4e53
vishanab <vishanab@vt.edu>
2026-03-04 23:24:24 -0500

    fixes

--- src/DNATree.java
+++ src/DNATree.java
@@ -1,7 +1,23 @@
 
 public class DNATree {
     private Node root;
+
     public DNATree() {
         root = LeafNode.EMPTY_LEAF;
+    }
+
+
+    public String print() {
+        return root.print(0);
+    }
+
+
+    public String printLengths() {
+        return root.printLengths(0);
+    }
+
+
+    public String printStats() {
+        return root.printStats(0);
     }
 }
