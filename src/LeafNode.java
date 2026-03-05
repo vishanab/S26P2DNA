ac186c626d0de1b0e5a6c78a46d39f0ae371a4bb
vishanab <vishanab@vt.edu>
2026-03-04 22:56:43 -0500

    Update LeafNode.java

--- src/LeafNode.java
+++ src/LeafNode.java
@@ -1,28 +1,47 @@
 
 public class LeafNode implements Node {
     public static final LeafNode EMPTY_LEAF = new LeafNode(null);
-
+    String info;
     public LeafNode(String s) {
-
+        this.info = s;
     }
 
 
     public Node insert(String s) {
-        return null;
+        if (info == null) {
+            info = s;
+            return this;
+        }
+        else if (info.equals(s)) {
+            return this;
+        }
+        InternalNode newNode = new InternalNode(EMPTY_LEAF);
+        newNode.insert(info);
+        newNode.insert(s);
+        return newNode;
     }
 
 
     public Node remove(String s) {
-        return null;
+        if (info != null && info.equals(s)) {
+            return EMPTY_LEAF;
+        }
+        return this;
     }
 
 
     public String print() {
-        return null;
+        if (info == null) {
+            return "";
+        }
+        return info + "\n";
     }
 
 
     public boolean search(String s) {
-        return false;
+        if (info == null) {
+            return false;
+        }
+        return info.equals(s);
     }
 }
