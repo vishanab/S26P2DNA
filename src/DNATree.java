b8caa61d10608f37e28112875793b8d32e60c9a1
sitalpaudel <sitalpaudel@vt.edu>
2026-03-04 23:32:22 -0500

    c

--- src/DNATree.java
+++ src/DNATree.java
@@ -5,4 +5,5 @@ public class DNATree {
         root = LeafNode.EMPTY_LEAF;
     }
     
+    
 }
--- src/InternalNode.java
+++ src/InternalNode.java
@@ -51,4 +51,5 @@ public class InternalNode implements Node{
             return 4;
         }
     }
+    
 }
